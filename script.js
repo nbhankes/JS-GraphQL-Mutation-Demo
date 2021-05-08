09208488de1f766a0656907fa061fe13c90b02c9
nbankes <nbhankes@gmail.com>
2021-05-08 13:20:35 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function myAlert() {
+    alert("Documents Successfuly Connected!");
+}
+
+const mutation = `
+    mutation {
+        changeUserStatus(input: {clientMutationId: "YOUR_USERNAME", emoji: ":rocket:", expiresAt: "2021-05-09T00:00:00", limitedAvailability: true,  message:"Working on an API tutorial"}) {
+                    clientMutationId
+                    status {
+                        message
+                        emoji
+                        updatedAt
+                    }
+            }    
+        }
+                `;
+
+
+const url = "https://api.github.com/graphql";
+let opts = {
+    method: "POST",
+    headers: { "Content-Type": "application/json", "Authorization": "Bearer PERSONAL_ACCESS_TOKEN" },
+    body: JSON.stringify({query: mutation })
+    };
+
+
+fetch(url, opts)
+  .then(res => res.json())
+  .then(console.log)
+  .catch(console.error);
